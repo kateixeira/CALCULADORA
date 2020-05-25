663e1fde24c6753fb770b57a725ee668e0343bbe
kateixeira <karina_teixeira@outlook.com.br>
2020-05-25 19:14:25 -0300

    Calculadora

---
+++ js/style.js
@@ -0,0 +1,69 @@
+// var displayBuffer = " ";
+//     var numero = " ";
+//     var termos = [undefined,undefined,undefined];
+//     var resultado = undefined;
+
+//     function pressNum(num){
+//         numero = numero.concat(num.innerHTML);
+//        showDisplay(num.innerHTML);
+
+//     }
+//     function clearMemory(){
+//         numero = " ";
+//         termos = [undefined, undefined, undefined];
+//         resultado = undefined;
+//     }
+//     function showDisplay(conteudo){
+//         displayBuffer = displayBuffer.concat(conteudo);
+//         var tela = document.getElementById("display");
+//         tela.value = displayBuffer;
+//     }
+//     function clearDisplay(){
+//         displayBuffer = " ";
+//         var tela = document.getElementById("display");
+//         tela.value = displayBuffer;
+//     }
+//     function clearAll(){
+//         clearDisplay();
+//         clearMemory();
+//     }
+//     function pressOperator(op){
+//         if(termos[1]==undefined){
+//          termos[0] = numero;
+//         termos[1] = op.innerHTML;
+//         showDisplay(op.innerHTML);
+//         numero= " ";
+
+//         }
+//         //fora da condiçao nada acontece 
+
+//     }
+//     function pressEqual(){
+//         if(termos[0] != undefined && termos[1] != undefined && numero != " "){
+//             termos[2] = numero;
+//         var keepresultado;
+
+//         switch(termos[1]){
+//             case '+':
+//             resultado = Number(termos[0])+Number(termos[2]);
+//                 break;
+//             case '-':
+//             resultado = Number(termos[0])-Number(termos[2]);
+//             break;
+//             case '*':
+//             resultado = Number(termos[0])*Number(termos[2]);
+//             break;
+//             case '/':
+//             resultado = Number(termos[0])/Number(termos[2]);
+//             break;
+
+//         }
+//         keepresultado = resultado;        
+//         clearDisplay();
+//         showDisplay(resultado);
+//         clearMemory();
+//         numero = keepresultado.ToString();
+//         }
+        
+
+//     }
